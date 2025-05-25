e8bf96f13055ee6318f2b668db70d49cb2dec833
aamna847 <160619359+aamna847@users.noreply.github.com>
2025-05-25 11:25:10 +0500

    Delete 06_Functions directory

--- 06_Functions/04_get_name.py
+++
@@ -1,9 +0,0 @@
-def get_name():
-    return "Sophia"
-
-def main():
-    name = get_name()
-    print(f"Howdy {name} ! 🤠")
-
-if __name__ == '__main__':
-    main()
--- 06_Functions/05_is_odd.py
+++
@@ -1,9 +0,0 @@
-def main():
-    for num in range(10, 20):
-        if num % 2 == 0:
-            print(f"{num} even")
-        else:
-            print(f"{num} odd")
-
-if __name__ == '__main__':
-    main()
