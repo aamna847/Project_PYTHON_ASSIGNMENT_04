4f19b2e57b49be1bdc0977bfedf467bfabd5f62c
aamna847 <160619359+aamna847@users.noreply.github.com>
2025-05-25 11:22:41 +0500

    Delete 00_intro_python directory

--- 00_intro_python/02_agreement_bot.py
+++
@@ -1,7 +0,0 @@
-def main():
-    animal=input("\033[1;3m What's your favorite animal _____?\033[0m]")
-    
-    print(f"My favourite animal is also {animal}")
-    
-if __name__ == "__main__":
-    main()
--- 00_intro_python/06_square_number.py
+++
@@ -1,8 +0,0 @@
-def main():
-    number= float(input("\033[1;3m Enter  a number to see its square: \033[0m"))
-    
-    square = number * number
-    print(f"\033[1;3m The square of {number} is {square} \033[0m")
-    
-if __name__ == "__main__":
-    main()
